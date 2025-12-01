d93c2706adca0eab9c28b9259b3171f698e6012a
Sushant Singh <sushant22007@gmail.com>
2025-12-01 15:06:59 +0530

    Refactor database connection: update connectDB function to use config for MongoDB URI

--- src/config/db.connection.js
+++ src/config/db.connection.js
@@ -1,6 +1,13 @@
 import mongoose from 'mongoose';
+import { config } from './env.js';
 
-export const connectDB = async (mongoURI) => {
-    try {
-        await mongoose.connect(mongoURI, { 
-            
+export const connectDB = async (req, res) => {
+    try{
+        await mongoose.connect(config.mongoUri)
+        .then(() => {
+            console.log("MongoDB connected successfully");
+        })
+    } catch (error) {
+        console.error("MongoDB connection failed:", error);
+    }
+}
